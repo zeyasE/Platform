fd36f6f3296f56d7da63aab0cfba243f698df9a9
zeyasE <zeya2542@gmail.com>
2020-08-17 16:34:56 +0700

    connected database

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const app = require("express")();
 const port = process.env.port || 3000;
+const mongoose = require("mongodb");
+const config = {
+  autoIndex: true,
+  useNewUrlParser: true,
+};
+const connectionBase =
+  "mongodb+srv://iwing:iwingku77@cluster0.ooosh.mongodb.net/iwing?retryWrites=true&w=majority";
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+mongoose
+  .connect(connectionBase, config)
+  .then(() => {
+    console.log("Database Connected!");
+  })
+  .catch(() => {
+    console.log("Cannot Connect to Database!!!");
+  });
 
 app.listen(port, () => {
   console.log(
