e8705684d7bc21cd89375877d287b9d2aa6a83d9
zeyasE <zeya2542@gmail.com>
2020-08-19 16:32:15 +0700

    test get&post

--- app.js
+++ app.js
@@ -3,30 +3,60 @@ const app = express();
 const port = process.env.port || 3000;
 const path = require("path");
 const pageNotFoundController = require("./controllers/page-not-found-controller.js");
-const MongoClient = require("mongodb").MongoClient;
+const dataUser = require("./models/datauser.js");
+const mongoose = require("mongoose");
 const uri =
   "mongodb+srv://iwing:iwingku77@cluster0.3abk6.mongodb.net/iwing?retryWrites=true&w=majority";
+// const MongoClient = require("mongodb").MongoClient;
 // const client = new MongoClient(uri, { useNewUrlParser: true });
 
 app.set("view engine", "ejs");
 app.set("views", "views");
+
 app.use(express.static("public"));
-// app.set("views", path.join(__dirname, "views"));
+app.use(express.json());
+
+const datauser = [{}];
+
+app.post("/testpost", async (req, res) => {
+  const payload = req.body;
+  const datauser = new dataUser(payload);
+  await datauser.save();
+  res.status(201).end();
+});
+
+app.get("/testget", async (req, res) => {
+  const datauser = await dataUser.find();
+  res.json(datauser);
+});
+app.get("/testget/:name", async (req, res) => {
+  const { name } = req.params;
+  const datauser = await dataUser.find({ name: name });
+  res.json(datauser);
+});
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 app.get("*", pageNotFoundController);
-// app.get("*", (req, res) => {
-//   res.render("page-not-found.ejs");
-// });
 
-MongoClient.connect(uri, (err, db) => {
-  // const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  if (!err) console.log("We are connected");
-  else console.log("Cannot Connect to Database!!!");
-  // client.close();
-});
+mongoose
+  .connect(uri, { useNewUrlParser: true })
+  .then(() => {
+    console.log("We are connected to Mongoose");
+  })
+  .catch((error) => {
+    console.log("MongoDB error", error);
+  });
+
+// mongoose.connection.on("error", (err) => {
+//   if (!err) console.log("We are connected");
+//   console.error("MongoDB error", err);
+// });
+// (uri, (err, db) => {
+//   if (!err) console.log("We are connected");
+//   else console.log("Cannot Connect to Database!!!");
+// });
 
 app.listen(port, () => {
   console.log(
---
+++ models/datauser.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+const datauserSchema = mongoose.Schema({
+  name: { type: String, required: true, unique: true },
+  type: { type: String, required: true },
+  status: { type: Boolean, required: true },
+  details: { type: [String], required: true },
+  idUser: { type: String, require: true },
+});
+datauserSchema.plugin(uniqueValidator);
+module.exports = mongoose.model("datauser", datauserSchema);
